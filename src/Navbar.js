c6455ec24153075961aa9c4f5fb29c6ec63cc4df
Sumit Singh <sumitsingh@Sumits-MacBook-Air.local>
2024-04-13 22:30:19 +0530

    minor change for cf2

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="container">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand mx-auto" href="#"><h1>SoftApply</h1></a>
+                <a className="navbar-brand mx-auto"><h1>SoftApply</h1></a>
                 <h6 className="mx-auto">Automatically apply to jobs without having to fill any forms</h6>
             </nav>
         </div>
