499bb7833c2aba987095e8f4c9838270ea7280d0
Sumit Singh <sumitsingh@Sumits-MacBook-Air.local>
2024-04-13 22:26:30 +0530

    minor change for cf

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
         <div className="container">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand mx-auto" href="#"><h1>SoftApply</h1></a>
-                <h6 className="mx-auto">Automatically apply to jobs without having to fill any form</h6>
+                <h6 className="mx-auto">Automatically apply to jobs without having to fill any forms</h6>
             </nav>
         </div>
     )
